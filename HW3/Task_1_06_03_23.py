61671033c48199a70439fccf7a4ceac4b4a40284
hp <pavlo.kulka@gmail.com>
2023-03-07 21:22:31 +0100

    My Robot Dreams Project from Lesson number 3

--- HW3/Task_1_06_03_23.py
+++ HW3/Task_1_06_03_23.py
@@ -1,19 +1,32 @@
+
 #завдання 1
+print("Завдання 1")
+print()
+
 print("I love Python \n" * 42)
 
 #Завдання 2
+print()
+print("Завдання 2")
 age_in_month = 324
+print(age_in_month)
 
 #Завдання 3
+print()
+print("Завдання 3")
 year = 12
 age_in_years = age_in_month / year
 print(age_in_years)
 
 #Завдання 4
+print()
+print("Завдання 4")
 my_age = "My name is {} and I'm {} years old".format("Pavlo", age_in_years)
 print(my_age)
 
 #Завдання 5
+print()
+print("Завдання 5")
 x = 4
 print(x > 5)
 print(x < 4)
@@ -22,6 +35,8 @@ print(x != 1)
 print(x >= 7)
 
 #Завдання 6
+print()
+print("Завдання 6")
 a = 2
 b = 5
 c = 6
