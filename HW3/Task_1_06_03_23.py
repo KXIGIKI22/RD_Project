7421461a57e3071cc01ddfc81eb029755246b84f
hp <pavlo.kulka@gmail.com>
2023-03-07 20:25:05 +0100

    My Robot Dreams Project from Lesson nymber 3

--- HW3/Task_1_06_03_23.py
+++ HW3/Task_1_06_03_23.py
@@ -1,1 +1,10 @@
 print("I love Python \n" * 42)
+
+age_in_month = 324
+year = 12
+age_in_years = age_in_month / year
+print(age_in_years)
+
+
+my_age = "My name is {} and I'm {} years old".format("Pavlo", age_in_years)
+print(my_age)
