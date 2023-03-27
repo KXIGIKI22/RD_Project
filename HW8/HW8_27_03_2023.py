a286b07ead1d8c8d93db92c3092d0b638a72743f
pavlo <pavlo.kulka@gmail.com>
2023-03-27 22:04:36 +0200

    My homework number 8

--- HW8/HW8_27_03_2023.py
+++ HW8/HW8_27_03_2023.py
@@ -4,3 +4,12 @@ set2 = {3, 4, 5, 6, 7}
 def intersection(set1, set2):
     return set1.intersection(set2)
 print(set1.intersection(set2))
+
+#Завдання 2
+set1 = {1, 2, 3, 4, 5, 6, 7}
+set2 = {3, 4, 5, 7, 8, 9}
+def unique_elements(set1, set2):
+    unique = set1 ^ set2
+    return list(unique)
+unique = unique_elements(set1, set2)
+print(unique)
